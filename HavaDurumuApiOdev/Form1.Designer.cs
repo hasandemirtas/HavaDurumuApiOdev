5179794d91ea7f37a295096a67150fe77bbe3d91
Hasan Demirtas <hasan.tayip.demirtas@gmail.com>
2022-07-17 23:56:58 +0300

    Add project files.

---
+++ HavaDurumuApiOdev/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace HavaDurumuApiOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetir = new FontAwesome.Sharp.IconButton();
+            this.cbSehirler = new System.Windows.Forms.ComboBox();
+            this.lblHavaDurumu = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.AutoSize = true;
+            this.btnGetir.BackColor = System.Drawing.Color.Green;
+            this.btnGetir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGetir.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGetir.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnGetir.IconChar = FontAwesome.Sharp.IconChar.Snowflake;
+            this.btnGetir.IconColor = System.Drawing.Color.White;
+            this.btnGetir.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnGetir.IconSize = 45;
+            this.btnGetir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGetir.Location = new System.Drawing.Point(268, 175);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(108, 53);
+            this.btnGetir.TabIndex = 0;
+            this.btnGetir.Text = "Getir";
+            this.btnGetir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGetir.UseVisualStyleBackColor = false;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // cbSehirler
+            // 
+            this.cbSehirler.FormattingEnabled = true;
+            this.cbSehirler.Location = new System.Drawing.Point(259, 234);
+            this.cbSehirler.Name = "cbSehirler";
+            this.cbSehirler.Size = new System.Drawing.Size(127, 21);
+            this.cbSehirler.TabIndex = 1;
+            // 
+            // lblHavaDurumu
+            // 
+            this.lblHavaDurumu.AutoSize = true;
+            this.lblHavaDurumu.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Bold);
+            this.lblHavaDurumu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHavaDurumu.Location = new System.Drawing.Point(416, 186);
+            this.lblHavaDurumu.Name = "lblHavaDurumu";
+            this.lblHavaDurumu.Size = new System.Drawing.Size(0, 21);
+            this.lblHavaDurumu.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(220, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hava Durumu Api Uygulması";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHavaDurumu);
+            this.Controls.Add(this.cbSehirler);
+            this.Controls.Add(this.btnGetir);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "HavaDurumuApiForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton btnGetir;
+        private System.Windows.Forms.ComboBox cbSehirler;
+        private System.Windows.Forms.Label lblHavaDurumu;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HavaDurumuApiOdev/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HavaDurumuApiOdev
+{
+    public partial class Form1 : Form
+    {
+        static String[] sehirler = new string[81]
+        {
+            "Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", 
+            "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir", 
+            "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", 
+            "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır",
+            "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", 
+            "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay",
+            "Isparta", "İçel (Mersin)", "İstanbul", "İzmir", "Kars",
+            "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli",
+            "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş",
+            "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu",
+            "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas",
+            "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa",
+            "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt",
+            "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan",
+            "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string sehir in sehirler)
+            {
+                cbSehirler.Items.Add(sehir);
+            }
+            cbSehirler.SelectedItem = "Adana";
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            lblHavaDurumu.Text = "Bekleyiniz...";
+            GetWeather();
+        }
+
+        private void GetWeather()
+        {
+            string api = "476ac7e8874328c04bd4cba59216539c";
+            string baglanti = "http://api.openweathermap.org/data/2.5/weather?q="+cbSehirler.SelectedItem+"&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument Hava = XDocument.Load(baglanti);
+            var sicaklik = Hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var durum = Hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            lblHavaDurumu.Text = cbSehirler.SelectedItem +" şehrinde \n Sıcaklık: " + sicaklik + " ºC \n Durum: "+ durum;
+        }
+    }
+}
